79230d0f1286cb6ea0224add12ce34dd186a08f6
Mension <72541408+dotmet@users.noreply.github.com>
2023-08-24 04:01:36 +0000

    Add CPP library Eigen

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 import sys
+import os
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup, find_packages, Extension
 
 __version__ = "0.0.1"
 
+root_dir = os.getcwd()
+include_dirs = [root_dir+'/pypolymer/include'] 
+
 ext_modules = [
-    
-    Pybind11Extension("pypolymer.util.topo_tools_cpp",
-        ["pypolymer/util/topoTools.cpp"],
+    Pybind11Extension("pypolymer.util",
+        ["pypolymer/util/topoTools.cpp", 'pypolymer/util/geometry_approximate.cpp'],
         define_macros = [('VERSION_INFO', __version__)], 
-        include_dirs = ["/usr/include/eigen3"],
-        ),   
-     
-    Pybind11Extension("pypolymer.util.geometry_approximation_cpp",
-        ["pypolymer/util/geometry_approximate.cpp"],
-        define_macros = [('VERSION_INFO', __version__)],
-        include_dirs = ["/usr/include/eigen3"],
-        ),
-]
+        include_dirs = include_dirs), ]
 
 setup(
     name="PyPolymer",
     version=__version__,
     author="Even Wong",
-    author_email="evenwong@stu.cdut.edu.cn",
-    url="https://github.com/???",
+    author_email="wdyphy@zju.edu.cn",
+    url="https://github.com/dotmet/pypolymer.git",
     description="A tookit for polymer construction & analysis.",
     long_description="",
     ext_modules=ext_modules,
@@ -34,13 +29,13 @@ setup(
     packages = find_packages(),
     zip_safe=False,
     python_requires=">=3.6",
-        install_requires = [
-        'matplotlib',
-        'numpy',
-        'scipy',
-        'numba',
-        'gsd',
-        'seaborn',
-        'pybind11[global]',
-        ],
+    install_requires = ['matplotlib',
+                        'numpy',
+                        'scipy',
+                        'numba',
+                        'gsd',
+                        'seaborn',
+                        'fresnel',
+                        'pillow',
+                        'tqdm']
 )
